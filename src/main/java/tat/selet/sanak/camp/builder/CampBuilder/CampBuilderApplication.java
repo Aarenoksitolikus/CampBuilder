fb2dc2b51d4919d4c9a1f8ca9adbd3a6130bfad4
Иванов Максим <mivanov@i-novus.ru>
2022-05-23 15:35:07 +0300

    Initial commit

---
+++ src/main/java/tat/selet/sanak/camp/builder/CampBuilder/CampBuilderApplication.java
@@ -0,0 +1,13 @@
+package tat.selet.sanak.camp.builder.CampBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tat/selet/sanak/camp/builder/CampBuilder/CampBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package tat.selet.sanak.camp.builder.CampBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
